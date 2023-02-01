56037576fcb427e06785a16098e84b415da9268e
sunni426 <sunni426@bu.edu>
2023-02-01 15:05:23 -0500

    profile: display files allocating most mem

--- mat_multiply.py
+++ mat_multiply.py
@@ -88,14 +88,19 @@ mat_multiply(mat1, mat2)
 
 # trace
 snapshot = tracemalloc.take_snapshot()
-top_stats = snapshot.statistics('traceback')
 
-# pick the biggest memory block
-stat = top_stats[0]
-print("%s memory blocks: %.1f KiB"%(stat.count,stat.size/1024))
-for line in stat.traceback.format():
-    print(line)
+# top_stats = snapshot.statistics('traceback')
+# # pick the biggest memory block
+# stat = top_stats[0]
+# print("%s memory blocks: %.1f KiB"%(stat.count,stat.size/1024))
+# for line in stat.traceback.format():
+#     print(line)
 
+# display the 10 files allocating the most memory
+top_stats = snapshot.statistics('lineno')
+print("[ Top 10 ]")
+for stat in top_stats[:10]:
+    print(stat)
 
 # if __name__ == '__main__':
 #     main()
